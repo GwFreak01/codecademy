e6e606b3625e566d672a4fd4f043034b4add5bb6
GWFreak01 <gwfreak01@gmail.com>
2018-08-04 14:06:52 -0400

    Format game board with printBoard

--- minesweeper/minesweeper.js
+++ minesweeper/minesweeper.js
@@ -20,4 +20,13 @@ let board = [
     [' ', ' ', ' ']
 ];
 
-console.log(board);
+const printBoard = (board) => {
+    console.log('CurrentBoard: ');
+    console.log(board[0].join(' | '));
+    console.log(board[1].join(' | '));
+    console.log(board[2].join(' | '));
+};
+
+
+// console.log(board);
+printBoard(board);

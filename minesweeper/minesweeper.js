70f37777a24a8320820efb9ef39df5268bfc8d5c
GWFreak01 <gwfreak01@gmail.com>
2018-08-05 22:43:25 -0400

    Dynamically generate player and bomb board.

--- minesweeper/minesweeper.js
+++ minesweeper/minesweeper.js
@@ -14,24 +14,56 @@
 // console.log(bombLine);
 // console.log(blankLine);
 
-let board = [
-    [' ', ' ', ' '],
-    [' ', ' ', ' '],
-    [' ', ' ', ' ']
-];
+// let board = [
+//     [' ', ' ', ' '],
+//     [' ', ' ', ' '],
+//     [' ', ' ', ' ']
+// ];
+//
+// const printBoard = (board) => {
+//     console.log('CurrentBoard: ');
+//     console.log(board[0].join(' | '));
+//     console.log(board[1].join(' | '));
+//     console.log(board[2].join(' | '));
+// };
+//
+//
+// // console.log(board);
+// printBoard(board);
+//
+// board[0][1] = '1';
+// board[2][2] = 'B';
+//
+// printBoard(board);
 
-const printBoard = (board) => {
-    console.log('CurrentBoard: ');
-    console.log(board[0].join(' | '));
-    console.log(board[1].join(' | '));
-    console.log(board[2].join(' | '));
+let generatePlayerBoard =[];
+
+generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+    let board = [];
+    for (let rowIndex = 0; rowIndex < numberOfRows; rowIndex++) {
+        let row = [];
+        for (let columnIndex = 0; columnIndex < numberOfColumns; columnIndex++) {
+            row.push(' ');
+        }
+        board.push(row);
+    }
+    console.log(board);
+    return board;
+    // generatePlayerBoard = board;
 };
+generatePlayerBoard(2, 3);
 
+// console.log(generatePlayerBoard);
 
-// console.log(board);
-printBoard(board);
-
-board[0][1] = '1';
-board[2][2] = 'B';
-
-printBoard(board);
+generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+    let board = [];
+    for (let rowIndex = 0; rowIndex < numberOfRows; rowIndex++) {
+        let row = [];
+        for (let columnIndex = 0; columnIndex < numberOfColumns; columnIndex++) {
+            row.push(null);
+        }
+        board.push(row);
+    }
+    console.log(board);
+    return board;
+};

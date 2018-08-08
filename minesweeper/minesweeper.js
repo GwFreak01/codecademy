9430c70413fd1dcbfd0259b3309cb319e16c225d
GWFreak01 <gwfreak01@gmail.com>
2018-08-08 16:12:35 -0400

    Dynamically generate player and bomb board.

--- minesweeper/minesweeper.js
+++ minesweeper/minesweeper.js
@@ -36,9 +36,9 @@
 //
 // printBoard(board);
 
-let generatePlayerBoard =[];
+// let generatePlayerBoard =[];
 
-generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
     let board = [];
     for (let rowIndex = 0; rowIndex < numberOfRows; rowIndex++) {
         let row = [];
@@ -47,15 +47,15 @@ generatePlayerBoard = (numberOfRows, numberOfColumns) => {
         }
         board.push(row);
     }
-    console.log(board);
+    // console.log(board);
     return board;
     // generatePlayerBoard = board;
 };
-generatePlayerBoard(2, 3);
+// generatePlayerBoard(2, 3);
 
 // console.log(generatePlayerBoard);
 
-generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
     let board = [];
     for (let rowIndex = 0; rowIndex < numberOfRows; rowIndex++) {
         let row = [];
@@ -64,6 +64,44 @@ generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
         }
         board.push(row);
     }
-    console.log(board);
+
+    let numberOfBombsPlaced = 0;
+    while (numberOfBombsPlaced <= numberOfBombs) {
+        // Potential to overwrite already existing bombs
+        let randomRowIndex = Math.floor(Math.random() * Math.floor(numberOfRows));
+        let randomColumnIndex = Math.floor(Math.random() * Math.floor(numberOfColumns));
+        // console.log('BombRow: %s \n BombColumn: %s', randomRowIndex, randomColumnIndex);
+        board[randomRowIndex][randomColumnIndex] = 'B';
+        numberOfBombsPlaced++;
+    }
+    // console.log(board);
+
     return board;
 };
+// generateBombBoard(2, 3, 3);
+
+// console.log(generatePlayerBoard);
+
+const printBoard = (board) => {
+    // console.log('CurrentBoard: ');
+    console.log(board.map((row) => row.join(' | ')).join('\n'));
+    // console.log(board[0].join(' | '));
+    // console.log(board[1].join(' | '));
+    // console.log(board[2].join(' | '));
+
+
+};
+
+
+// console.log(printBoard(generatePlayerBoard(2,3)));
+
+let playerBoard = generatePlayerBoard(3, 4);
+
+let bombBoard = generateBombBoard(3, 4, 5);
+
+console.log('Player Board: ');
+printBoard(playerBoard);
+console.log('Bomb Board: ');
+printBoard(bombBoard);
+
+

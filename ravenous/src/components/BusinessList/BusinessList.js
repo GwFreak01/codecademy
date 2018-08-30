979826ad366b357d848669c482f28f2de38b5350
GWFreak01 <gwfreak01@gmail.com>
2018-08-30 03:20:15 -0500

    Creates the BusinessList Component

--- ravenous/src/components/BusinessList/BusinessList.js
+++ ravenous/src/components/BusinessList/BusinessList.js
@@ -2,3 +2,12 @@ import React from 'react';
 import './BusinessList.css';
 import Business from '../Business/Business';
 
+class BusinessList extends React.Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}

61713b98427efe7b95fa9841d77ea417339365d9
GWFreak01 <gwfreak01@gmail.com>
2018-08-30 02:50:38 -0500

    Create initial Ravenous project directory

--- ravenous/src/App.js
+++ ravenous/src/App.js
@@ -11,8 +11,6 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-            Hello Codecademy!
-            <br/>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>

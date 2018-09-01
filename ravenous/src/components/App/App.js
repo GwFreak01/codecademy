cb0fcd233dba4f754b21966233030f712b9a59a1
GWFreak01 <gwfreak01@gmail.com>
2018-09-01 00:11:22 -0500

    Renders Ravenous

--- ravenous/src/components/App/App.js
+++ ravenous/src/components/App/App.js
@@ -7,7 +7,11 @@ import SearchBar from '../SearchBar/SearchBar';
 class App extends Component {
   render() {
     return (
-
+        <div className="App">
+            <h1>ravenous</h1>
+          <SearchBar/>
+          <BusinessList/>
+        </div>
     );
   }
 }

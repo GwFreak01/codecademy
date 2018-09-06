4f0191a87344797559c7c5ab4a8f57529b84d44b
GWFreak01 <gwfreak01@gmail.com>
2018-09-06 04:00:33 -0500

    Removes hardcoded business and businesses information

--- ravenous/src/components/App/App.js
+++ ravenous/src/components/App/App.js
@@ -5,26 +5,26 @@ import BusinessList from '../BusinessList/BusinessList';
 import SearchBar from '../SearchBar/SearchBar';
 import Yelp from '../../util/Yelp';
 
-const business = {
-    imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
-    name: 'MarginOtto Pizzeria',
-    address: '1010 Paddington Way',
-    city: 'Flavortown',
-    state: 'NY',
-    zipCode: '10101',
-    category: 'Italian',
-    rating: 4.5,
-    reviewCount: 90
-};
-
-const businesses = [
-    business,
-    business,
-    business,
-    business,
-    business,
-    business
-];
+// const business = {
+//     imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+//     name: 'MarginOtto Pizzeria',
+//     address: '1010 Paddington Way',
+//     city: 'Flavortown',
+//     state: 'NY',
+//     zipCode: '10101',
+//     category: 'Italian',
+//     rating: 4.5,
+//     reviewCount: 90
+// };
+//
+// const businesses = [
+//     business,
+//     business,
+//     business,
+//     business,
+//     business,
+//     business
+// ];
 
 class App extends Component {
     constructor(props) {

1157b764765cb8e1b16a1c9b4f7aae532d7a47ec
GWFreak01 <gwfreak01@gmail.com>
2018-10-20 17:55:23 -0500

    Adding Hard-Coded initial value for searchResults

--- jammming/src/components/App/App.js
+++ jammming/src/components/App/App.js
@@ -8,20 +8,41 @@ import SearchResults from '../SearchResults/SearchResults';
 import Playlist from '../Playlist/Playlist';
 
 class App extends Component {
-  render() {
-    return (
-        <div>
-            <h1>Ja<span className="highlight">mmm</span>ing</h1>
-            <div className="App">
-                <SearchBar/>
-                <div className="App-playlist">
-                    <SearchResults/>
-                    <Playlist/>
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            searchResults: [
+                {
+                    name: "Grass",
+                    artist: "Cow",
+                    album: "Greener",
+                    id: "0"
+
+                },
+                {
+                    name: "God's Plan",
+                    artist: "Drake",
+                    album: "Scorpion",
+                    id: "1"
+                }
+            ]
+        };
+    }
+    render() {
+        return (
+            <div>
+                <h1>Ja<span className="highlight">mmm</span>ing</h1>
+                <div className="App">
+                    <SearchBar/>
+                    <div className="App-playlist">
+                        <SearchResults/>
+                        <Playlist/>
+                    </div>
                 </div>
             </div>
-        </div>
-    );
-  }
+        );
+    }
 }
 
 export default App;

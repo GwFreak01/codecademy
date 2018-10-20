bf8f4d280ebed670184d7ad12d3f5df049f1673c
GWFreak01 <gwfreak01@gmail.com>
2018-10-20 17:12:53 -0500

    Changing directory pointer to background image. Creating render function for Playlist and export.

--- jammming/src/components/Playlist/Playlist.js
+++ jammming/src/components/Playlist/Playlist.js
@@ -1,5 +1,16 @@
-<div className="Playlist">
-    <input value="New Playlist"/>
-    <!-- Add a TrackList component -->
-    <a className="Playlist-save">SAVE TO SPOTIFY</a>
-</div>
+import React from 'react';
+
+import TrackList from '../TrackList/TrackList';
+class Playlist extends React.Component {
+    render() {
+        return (
+            <div className="Playlist">
+                <input defaultValue={'New Playlist'}/>
+                {/*<TrackList/>*/}
+                <a className="Playlist-save">SAVE TO SPOTIFY</a>
+            </div>
+        );
+    }
+}
+
+export default Playlist;

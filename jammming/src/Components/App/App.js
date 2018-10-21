5449113431600d26492c9827096f4872d6d05207
GWFreak01 <gwfreak01@gmail.com>
2018-10-21 01:34:13 -0500

    Setting default playlistName

--- jammming/src/Components/App/App.js
+++ jammming/src/Components/App/App.js
@@ -12,7 +12,7 @@ class App extends Component {
 
         this.state = {
             searchResults: [],
-            playListName: "Lit Time",
+            playListName: "New Playlist",
             playlistTracks: []
         };
 

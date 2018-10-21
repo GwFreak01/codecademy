d2c36cdcf366d851f9547dd45e78713ea00c8a3c
GWFreak01 <gwfreak01@gmail.com>
2018-10-20 20:28:02 -0500

    Creating handleNameChange function to handle onChange event of input to pass to updatePlaylistName property.

--- jammming/src/Components/Playlist/Playlist.js
+++ jammming/src/Components/Playlist/Playlist.js
@@ -3,10 +3,21 @@ import './Playlist.css';
 import TrackList from '../TrackList/TrackList';
 
 class Playlist extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.handleNameChange = this.handleNameChange.bind(this);
+    }
+
+    handleNameChange(e) {
+        let updatedName = e.target.value;
+        this.props.onNameChange(updatedName);
+    }
+
     render() {
         return (
             <div className="Playlist">
-                <input defaultValue={'New Playlist'}/>
+                <input defaultValue={'New Playlist'} onChange={this.handleNameChange}/>
                 <TrackList
                     tracks={this.props.playlistTracks}
                     onRemove={this.props.onRemove}
